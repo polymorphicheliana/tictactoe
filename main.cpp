3aee43fa0e09804921d9ee9e5e7fa1e0a3e3ea7d
Polymorphicheliana <helianakaklamani@gmail.com>
2026-03-08 20:42:50 +0200

    implemented moves, turns and second player

--- main.cpp
+++ main.cpp
@@ -51,13 +51,85 @@ void printBoard()
     }
 }
 
+char readCharacter()
+{
+    const unsigned int maxSize = 128;
+    char inputLine[maxSize];
+    std::fgets(inputLine, maxSize, stdin);
+    return inputLine[0];
+}
+
+unsigned int getMove(bool xturn)
+{
+    char move;
+    char playerTurn;
+
+    if(xturn)
+    {
+        playerTurn = 'X';    
+    }
+    else 
+    {
+        playerTurn = 'O';
+    }
+    printf("Choose your move (%c) : ", playerTurn);
+    move = readCharacter();
+//    printf("You entered : '%c'\n", move);
+    while(true)
+    {
+        if(move < '1' || move > '9')
+        {
+            printf("Please choose a number from the board now... bitch... (%c) : ", playerTurn);
+            move = readCharacter();
+        }
+        else if(board[move - '1'] != State::Empty)
+        {
+            printf("(%c) are you trying to be funny? right???, please choose an empty cell : ", playerTurn);
+            move = readCharacter();
+        }
+        else
+        {
+            return move - '1';
+        }
+    }
+}
+
+void gameLoop()
+{
+    unsigned int counter = 0;
+    bool xTurn;
+
+    while(counter < cellCount)
+    {
+        
+        if(counter % 2 == 0)
+        {
+            xTurn = true;
+        }
+        else
+        {
+            xTurn = false;
+        }
+        printBoard();
+        unsigned int move = getMove(xTurn);
+        if(xTurn)
+        {
+            board[move] = State::X;
+        }
+        else
+        {
+            board[move] = State::O;
+        }
+        counter = counter + 1;
+    }
+}
 int main()
 {
     setup();
-
+    gameLoop();
 //    board[5] = State::X;
 
-    printBoard();
+    
 
     return 0;
 }
